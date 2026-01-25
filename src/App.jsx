c2404a1b4a4d36e50778c09afb3730ff1df0e3d9
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2026-01-25 18:10:26 +0530

    Initial Login , Signup UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+// cognigen-frontend/src/App.jsx
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Auth/Login.jsx";
+import Signup from "./pages/Auth/Signup.jsx";
+import Dashboard from "./pages/Dashboard/sample.jsx";
 
-const App = () => {
-  return <div className="text-amber-200 text-9xl">App</div>;
-};
+function App() {
+  return (
+    <>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* private Routes */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Default redirect */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Catch all unknown routes */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
